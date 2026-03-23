804c73322d6a3a7d1980c388ae75cfa05d76a44f
Abenezer <154122424+Abenezer-se@users.noreply.github.com>
2026-03-23 23:36:03 +0300

    Update schedule_item.dart

--- lib/widgets/schedule_item.dart
+++ lib/widgets/schedule_item.dart
@@ -1,17 +1,1 @@
-import 'package:flutter/material.dart';
 
-class ScheduleItem extends StatelessWidget {
-  final String name;
-  final String time;
-
-  const ScheduleItem({super.key, required this.name, required this.time});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(name),
-      subtitle: Text(time),
-      trailing: const Icon(Icons.check),
-    );
-  }
-}
