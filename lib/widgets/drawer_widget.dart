eaba44a8054ac71d5278644271c55e73431b4765
eyutaa <eyerusalemberihun333@gmail.com>
2026-05-03 21:10:59 +0300

    Updated drawer_widget

--- lib/widgets/drawer_widget.dart
+++ lib/widgets/drawer_widget.dart
@@ -1,4 +1,4 @@
-// lib/widgets/drawer_widget.dart
+ // lib/widgets/drawer_widget.dart
 import 'package:flutter/material.dart';
 import '../constants/colors.dart';
 import '../screen/setting_screen.dart';
@@ -120,3 +120,53 @@ class AppDrawer extends StatelessWidget {
                             builder: (_) => const EmergencyScreen()));
                   },
                 ),
+                drawerItem(
+                  Icons.settings,
+                  "Settings",
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (_) => const SettingsScreen()));
+                  },
+                ),
+              ],
+            ),
+          ),
+
+          // Footer
+          Container(
+            margin: const EdgeInsets.all(16),
+            padding: const EdgeInsets.all(12),
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: const Color(0xFF2EC4B6).withOpacity(0.1),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: const Center(
+              child: Text(
+                "Stay Healthy",
+                style: TextStyle(fontWeight: FontWeight.w500),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget drawerItem(IconData icon, String title,
+      {bool active = false, VoidCallback? onTap}) {
+    return ListTile(
+      leading:
+          Icon(icon, color: active ? const Color(0xFF2EC4B6) : Colors.grey),
+      title: Text(
+        title,
+        style:
+            TextStyle(color: active ? const Color(0xFF2EC4B6) : Colors.black),
+      ),
+      onTap: onTap,
+    );
+  }
+}
